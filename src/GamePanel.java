06442b53211aded6873d938a88ef74b7ed03eb10
Diego Santana <diego_4palco@hotmail.com>
2019-09-12 14:51:03 -0300

    Matriz em linhas da janela

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,8 +1,9 @@
 import java.awt.Dimension;
+import java.awt.Graphics;
 
 import javax.swing.JPanel;
 
-public class GamePanel extends JPanel {
+public class GamePanel extends JPanel implements Runnable {
 	
 	private static final long serialVersionUID = 1L;
 	
@@ -10,6 +11,30 @@ public class GamePanel extends JPanel {
 
 	public GamePanel() {
 		setPreferredSize(new Dimension(WIDHT, HEIGHT));
+	}
+	
+	public void start() {
+		
+	}
+	public void stop() {
+		
+	}
+	
+	public void tickt() {
+		
+	}
+	
+	public void paint(Graphics g) {
+		for(int i = 0; i < WIDHT/10; i++) {
+			g.drawLine(i * 10, 0, i * 10, HEIGHT);
+		}
+		for(int i = 0; i < HEIGHT/10; i++) {
+			g.drawLine(0, i * 10, HEIGHT, i * 10);
+		}
+	}
+	
+	public void run() {
+		
 	}
 
 }
